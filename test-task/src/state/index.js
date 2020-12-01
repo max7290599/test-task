760cb0b49db70bc4a187d3d2d70a0dc64d95c4ec
max7290599 <atroshschanka.maksim@gmail.com>
2020-12-01 10:29:03 +0300

    fix

--- test-task/src/state/index.js
+++ test-task/src/state/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import meta from './meta-reducer';
 import changeForm from "./form-reducer";
-import { dialogWindow } from "./dialogWindow";
+import dialogWindow from "./dialogWindow";
 
 export default combineReducers({
     meta,
